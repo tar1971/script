c614834232fd3af2d29b03ef0d07739bc6fbfc60
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-05-19 00:35:53 +0200

    Add files via upload

---
+++ installer2.sh
@@ -0,0 +1,25 @@
+#!/bin/sh # 
+ # # Command: wget https://raw.githubusercontent.com/tar1971/script/main/installer2.sh -qO - | /bin/sh # # ########################################### ###########################################  
+#!/bin/sh
+ # 
+# script download my scripts #  
+cd /tmp
+set -e 
+curl  -k -Lbk -m 55532 -m 555104 "https://drive.google.com/uc?id=1gCxV7Ww1DSC1A-BERbH1c4aDfaJteshd&export=download" > /tmp/script.tar.gz
+sleep 1
+wait
+tar -xzf script.tar.gz  -C /
+wait
+cd ..
+set +e
+rm -f /tmp/script.tar.gz
+echo "   UPLOADED BY  >>>>   TAR_TAR "   
+sleep 4;                                                                                                                  
+echo ". >>>>         RESTARING     <<<<"
+echo "**********************************************************************************"
+wait
+killall -9 enigma2
+exit 0
+
+
+
